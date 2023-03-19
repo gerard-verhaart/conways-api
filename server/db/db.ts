04d28999f6db90023b276bfed7393fb0916eeac0
Ryan-Kendrick <118949957+Ryan-Kendrick@users.noreply.github.com>
2023-03-20 11:26:27 +1300

    step 1

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,3 +1,6 @@
 import connection from './connection'
+import { Post } from '../../common/post'
 
-export function getAllPosts(db = connection): void {}
+export function getAllPosts(db = connection): Promise<Post[]> {
+  return db('posts').select('*', 'date_created AS dateCreated')
+}
--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,5 +1,16 @@
-import express from 'express'
+import { Router } from 'express'
+import { getAllPosts } from '../db/db'
 
-const router = express.Router()
+const router = Router()
+
+router.get('/', (req, res) => {
+  getAllPosts()
+    .then((posts) => {
+      res.json(posts)
+    })
+    .catch((err: Error) => {
+      res.status(500).send(err.message)
+    })
+})
 
 export default router
